e0367dc6e4f8a7d5cdc2e68fbca85e4c737cae07
Cyprien Poisson <c.poisson@3h15.com>
2017-07-06 11:05:28 +0200

    build tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,47 @@ var sass = require('gulp-sass');
 const connect = require('gulp-connect');
 
 
+// COPY
 
+/**
+ * Copy files from monster template
+ */
+gulp.task('copy:monster', () => {
+  return gulp.src([
+      // './monster_template/assets/**/*',
+      './monster_template/css/**/*',
+      './monster_template/fonts/**/*',
+      './monster_template/images/**/*',
+      './monster_template/js/**/*',
+    ], {base: './monster_template'})
+    .pipe(gulp.dest('./dist/'));
+});
 
+/**
+ * Copy assets from /src
+ */
+gulp.task('copy:assets', () => {
+  return gulp.src([
+      './src/assets/**/*',
+    ], {base: './src/'})
+    .pipe(gulp.dest('./dist/'));
+});
 
-gulp.task('html', () => {
+// BUILD
+
+/**
+ * Build sass file from /src and copy.
+ */
+gulp.task('build:sass', function () {
+  return gulp.src('./src/css/overrides.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css/'));
+});
+
+/**
+ * Build html from hbs and copy.
+ */
+gulp.task('build:html', () => {
   return gulp.src('./src/pages/**/*.hbs')
     .pipe(handlebars({}, {
       ignorePartials: true,
@@ -28,41 +65,56 @@ gulp.task('html', () => {
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('monster_template', () => {
-  return gulp.src([
-      './monster_template/assets/**/*',
-      './monster_template/css/**/*',
-      './monster_template/fonts/**/*',
-      './monster_template/images/**/*',
-      './monster_template/js/**/*',
-    ], {base: './monster_template'})
-    .pipe(gulp.dest('./dist/'));
-});
+/**
+ * Full build process
+ */
+gulp.task('build:full', ['copy:monster', 'copy:assets', 'build:sass', 'build:html']);
 
-gulp.task('sass', function () {
-  return gulp.src('./src/css/overrides.scss')
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css/'));
-});
-
-gulp.task('build', ['html', 'monster_template', 'sass', 'reload']);
-
-gulp.task('reload', ['html', 'sass', 'monster_template'], function () {
-  gulp.src('./dist/*')
-    .pipe(connect.reload());
-});
+/**
+ * Partial build. Only process source files. (HTML & SASS)
+ */
+gulp.task('build:sources', ['build:sass', 'build:html']);
 
 
-gulp.task('connect', function() {
+// SERVE
+
+/**
+ * Serve files from /dist
+ */
+gulp.task('http:serve', function() {
   connect.server({
     root: 'dist',
     livereload: true
   });
 });
 
-gulp.task('watch', function () {
-  gulp.watch(['./src/**/*'], ['build']);
+/**
+ * Livereload files
+ */
+gulp.task('http:reload', function () {
+  gulp.src('./dist/*').pipe(connect.reload());
 });
 
-gulp.task('default', ['connect', 'watch']);
+
+// WATCH
+
+
+/**
+ * Triggers livereload on file changes
+ */
+gulp.task('watch:reload', ['build:full'], function () {
+  gulp.watch(['./dist/**/*'], ['http:reload']);
+});
+
+/**
+ * Triggers sources build on file changes.
+ */
+gulp.task('watch:sources', ['build:full'], function () {
+  gulp.watch(['./src/**/*'], ['build:sources']);
+});
+
+
+// DEFAULT
+
+gulp.task('default', ['build:full', 'http:serve', 'watch:sources', 'watch:reload']);
 
